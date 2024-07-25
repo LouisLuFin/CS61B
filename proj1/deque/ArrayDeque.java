a36cd5cf1eb61ab023e5081205e4a04220c6f519
LouisLuFin <louislzh@gmail.com>
2024-07-25 21:10:12 +0800

    proj1 Checkpoint

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -3,17 +3,167 @@ package deque;
 public class ArrayDeque<listType> {
     private listType[] items;
     private int size;
+    private int nextFirst;
+    private int nextLast;
+
+    private int UnitSize;
+    private final int sizeMultiplier =10;
+
+    private final int minUnitSize=8;
 
     public ArrayDeque(){
-        items=(listType[]) new Object[8];
+        UnitSize=8;
+        items=(listType[]) new Object[UnitSize];
+        for (int i=0;i< items.length;i+=1){
+            items[i]=null;
+        }
         size=0;
+        nextFirst=2;
+        nextLast=3;
     }
 
-    private void resize(int capacity){
+    private void expandSize(int capacity){
+        if (size!=UnitSize){
+            throw new IllegalArgumentException("items not full yet, should not call expandSize()");
+        }
         listType[] a=(listType[]) new Object[capacity];
-        System.arraycopy(items,0,a,0,size);
+        for (int i=0;i< a.length;i+=1){
+            a[i]=null;
+        }
+        int first=nextFirst+1;
+        if (first>= items.length){
+            first=0;
+        }
+        int curridx=first;
+        int destPos=(sizeMultiplier /2-1)*UnitSize;
+        for (int i=0;i<size;i+=1){
+            a[destPos]=items[curridx];
+            curridx += 1;
+            if (curridx >= items.length) {
+                curridx = 0;
+            }
+            destPos+=1;
+        }
         items=a;
+        nextFirst=(sizeMultiplier /2-1)*UnitSize-1;
+        nextLast= destPos;
+        UnitSize=UnitSize* sizeMultiplier;
     }
 
+    private void extractSize() {
+        int capacity;
+        if (items.length / sizeMultiplier *6 >= minUnitSize && size< items.length/ sizeMultiplier *6) {
+            capacity = items.length / sizeMultiplier *6;
+        } else {
+            throw new IllegalArgumentException("capacity below minimum Unit Size or larger than 60% of original length, should not call extractSize()");
+        }
+
+        listType[] a = (listType[]) new Object[capacity];
+        for (int i = 0; i < a.length; i += 1) {
+            a[i] = null;
+        }
+        int first = nextFirst + 1;
+        if (first >= items.length) {
+            first = 0;
+        }
+        int curridx = first;
+        int destPos = UnitSize/sizeMultiplier;
+        for (int i = 0; i < size; i += 1) {
+            a[destPos] = items[curridx];
+            curridx += 1;
+            if (curridx >= items.length) {
+                curridx = 0;
+            }
+            destPos += 1;
+        }
+        items = a;
+        nextFirst = UnitSize/sizeMultiplier-1;
+        nextLast = destPos;
+        UnitSize = UnitSize / sizeMultiplier*6;
+    }
+
+    public listType removeFirst(){
+        if (size-1<UnitSize/sizeMultiplier*4){
+            extractSize();
+        }
+        nextFirst+=1;
+        if (nextFirst>=UnitSize){
+            nextFirst=0;
+        }
+        listType ret_val=items[nextFirst];
+        items[nextFirst]=null;
+        size-=1;
+        return ret_val;
+    }
+
+    public listType removeLast(){
+        if (size-1<UnitSize/sizeMultiplier*4){
+            extractSize();
+        }
+        nextLast-=1;
+        if (nextLast<0){
+            nextLast=items.length-1;
+        }
+        listType ret_val=items[nextLast];
+        items[nextLast]=null;
+        size-=1;
+        return ret_val;
+    }
+
+
+    public int size(){
+        return size;
+    }
+
+    public listType get(int index){
+        return items[index];
+    }
+
+    public boolean isEmpty(){
+        return size()==0;
+    }
+
+    public void addLast(listType added){
+        if (size==UnitSize){
+            this.expandSize(sizeMultiplier *UnitSize);
+        }
+        items[nextLast]=added;
+        size+=1;
+        nextLast+=1;
+        if (nextLast>=items.length){
+            nextLast=0;
+        }
+    }
+
+    public void addFirst(listType added){
+        if (size==UnitSize){
+            this.expandSize(sizeMultiplier *UnitSize);
+        }
+        items[nextFirst]=added;
+        size+=1;
+        nextFirst-=1;
+        if (nextFirst<0){
+            nextFirst=UnitSize-1;
+        }
+    }
+
+
+    public void printDeque(){
+        int first=nextFirst+1;
+        int last=nextLast-1;
+        if (first>= items.length){
+            first=0;
+        }
+
+        int curridx=first;
+        for (int i=0;i<size;i+=1){
+            System.out.print(items[curridx]+" ");
+            curridx+=1;
+            if (curridx>=items.length){
+                curridx=0;
+            }
+        }
+        System.out.println("\n==============");
+    }
 
 }
--- proj1/deque/ArrayDequeTest.java
+++ proj1/deque/ArrayDequeTest.java
@@ -1,2 +1,37 @@
-package deque;public class ArrayDequeTest {
+package deque;
+
+import org.junit.Test;
+
+public class ArrayDequeTest {
+    @Test
+    public void testArrayDeque(){
+        ArrayDeque<Integer> ad=new ArrayDeque<>();
+        ad.addFirst(3);
+        ad.addFirst(358);
+        ad.addFirst(8);
+        ad.addLast(96);
+        ad.addLast(36);
+        ad.addLast(98);
+        ad.addLast(79);
+        ad.addFirst(12);
+        ad.addFirst(15);
+        ad.printDeque();
+        System.out.println(ad.size());
+        System.out.println(ad.get(0));
+        System.out.println(ad.get(1));
+        System.out.println(ad.get(36));
+        for (int i=0;i<10000;i+=1){
+            ad.addFirst(i);
+        }
+        for (int i=10000;i>0;i-=1){
+            ad.addLast(i);
+            i-=1;
+        }
+        for (int i=0;i<5000;i+=1){
+            ad.removeFirst();
+        }
+        for (int i=0;i<5000;i+=1){
+            ad.removeLast();
+        }
+    }
 }
