03cb285730f45c1fb1e9e50c087630b0795fae0f
LouisLuFin <louislzh@gmail.com>
2024-08-12 17:08:13 +0800

    change to bug version

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -4,16 +4,9 @@
 public class Collatz {
 
     /** Buggy implementation of nextNumber! */
-    public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
-        }
+
     }
-/*    public static int nextNumber(int n) {
+    public static int nextNumber(int n) {
         if (n  == 1) {
             return 1;
         } else if (n % 2 == 0) {
@@ -22,7 +15,7 @@ public class Collatz {
             return 3*n+1;
         }
     }
-*/
+
     public static void main(String[] args) {
         int n = 5;
         System.out.print(n + " ");
